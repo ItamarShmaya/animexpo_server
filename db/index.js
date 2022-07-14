4bc44e38efb7b0cf9daf2a492887f9951083c742
Itamar <itamarshmaya@gmail.com>
2022-07-14 15:57:20 +0300

    notifications fix

--- db/index.js
+++ db/index.js
@@ -4,27 +4,27 @@ import {
   MONGO_ATLAS_PASSWORD,
 } from "../config/env_var.js";
 
-// mongoose.connect(
-//   "mongodb://127.0.0.1:27017/AnimExpo",
-//   (error, mongoDBInstance) => {
-//     if (error) throw new Error("Mongo Connection Error: " + error);
-//     if (!process.env.NODE_ENV) {
-//       const { host, port, name } = mongoDBInstance;
-//       console.log({ host, port, name });
-//     }
-//   },
-//   {
-//     useCreateIndex: true,
-//     autoIndex: true,
-//   }
-// );
-
-const URI = `mongodb+srv://${MONGO_ATLAS_USERNAME}:${MONGO_ATLAS_PASSWORD}@animeexpo.6seaw.mongodb.net/AnimeExpo?retryWrites=true&w=majority`;
-
-mongoose.connect(URI, (error, mongoDBInstance) => {
-  if (error) throw new Error("Mongo Connection Error: " + error);
-  if (!process.env.NODE_ENV) {
-    const { host, port, name } = mongoDBInstance;
-    console.log({ host, port, name });
+mongoose.connect(
+  "mongodb://127.0.0.1:27017/AnimExpo",
+  (error, mongoDBInstance) => {
+    if (error) throw new Error("Mongo Connection Error: " + error);
+    if (!process.env.NODE_ENV) {
+      const { host, port, name } = mongoDBInstance;
+      console.log({ host, port, name });
+    }
+  },
+  {
+    useCreateIndex: true,
+    autoIndex: true,
   }
-});
+);
+
+// const URI = `mongodb+srv://${MONGO_ATLAS_USERNAME}:${MONGO_ATLAS_PASSWORD}@animeexpo.6seaw.mongodb.net/AnimeExpo?retryWrites=true&w=majority`;
+
+// mongoose.connect(URI, (error, mongoDBInstance) => {
+//   if (error) throw new Error("Mongo Connection Error: " + error);
+//   if (!process.env.NODE_ENV) {
+//     const { host, port, name } = mongoDBInstance;
+//     console.log({ host, port, name });
+//   }
+// });
